bad33b13369a8e2162cd4002b3346cfca16a6fbf
Peeranut Mahatham <robin_43499@hotmail.com>
2016-07-20 16:02:25 +0700

    Initial Commit

---
+++ LenkilaUser/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LenkilaUser
+//
+//  Created by Peeranut Mahatham on 7/20/2559 BE.
+//  Copyright © 2559 Peeranut Mahatham. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
